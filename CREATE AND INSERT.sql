830100d6ae76310121d9550a81d52b7680dcc6d4
BhuvaneswariMohan <bhuvaneswarimohan023@gmail.com>
2017-01-23 10:17:09 +0530

    First commit

---
+++ CREATE AND INSERT.sql
@@ -0,0 +1,96 @@
+CREATE TABLE `session_schedule` ( 
+`ID` INT(10) NOT NULL, 
+`FOOD_TYPE` VARCHAR(20) NOT NULL, 
+`START_TIME` TIME NOT NULL,
+ `END_TIME` TIME NOT NULL,  PRIMARY KEY (`ID`));
+
+INSERT INTO `session_schedule` (`ID`, `FOOD_TYPE`, `START_TIME`, `END_TIME`) VALUES('1','Breakfast','08:00:00','11:00:00');
+INSERT INTO `session_schedule` (`ID`, `FOOD_TYPE`, `START_TIME`, `END_TIME`) VALUES('2','Lunch','11:15:00','15:00:00');
+INSERT INTO `session_schedule` (`ID`, `FOOD_TYPE`, `START_TIME`, `END_TIME`) VALUES('3','Refreshment','15:15:00','23:00:00');
+INSERT INTO `session_schedule` (`ID`, `FOOD_TYPE`, `START_TIME`, `END_TIME`) VALUES('4','Dinner','19:00:00','23:00:00');
+
+
+
+CREATE TABLE `menu_items` (  
+`ID` INT(20) NOT NULL, 
+ `FOOD_ITEMS` VARCHAR(20) NOT NULL, 
+`RATE` INT(30) NOT NULL,  PRIMARY KEY (`ID`));
+
+
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('1','Idly','10');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('2','Dosa','25');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('3','Vada','10');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('4','Poori','30');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('5','Coffee-B','25');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('6','Tea-B','25');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('7','Pongal','30');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('8','South Indian Meals','120');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('9','North Indian Thali','180');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('10','Variety rice','90');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('11','Coffee-L','25');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('12','Tea-L','25');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('13','Snacks','50');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('14','Fried rice','80');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('15','Chappati','80');
+INSERT INTO `menu_items` (`ID`, `FOOD_ITEMS`, `RATE`) VALUES('16','Chat items','50');
+
+
+
+
+CREATE TABLE `item_quantity` ( 
+ `ID` INT(30) NOT NULL AUTO_INCREMENT, 
+ `FOOD_TYPE` INT(30) DEFAULT NULL,
+ `MENU_TYPE` INT(30) DEFAULT NULL,
+`QTY` INT(30) DEFAULT NULL,  PRIMARY KEY (`ID`), 
+ KEY `RT_TYPE` (`FOOD_TYPE`),
+ KEY `RT_MENU` (`MENU_TYPE`),
+ CONSTRAINT `RT_MENU` FOREIGN KEY (`MENU_TYPE`) REFERENCES `menu_items` (`ID`),  CONSTRAINT `RT_TYPE` FOREIGN KEY (`FOOD_TYPE`) REFERENCES `session_schedule` (`ID`))
+
+
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('1','1','1','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('2','1','2','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('3','1','3','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('4','1','4','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('5','1','5','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('6','1','6','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('7','1','7','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('8','2','8','75');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('9','2','9','75');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('10','2','10','75');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('11','3','6','200');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('12','3','7','200');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('13','3','11','200');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('14','4','12','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('15','4','13','100');
+INSERT INTO `item_quantity` (`ID`, `FOOD_TYPE`, `MENU_TYPE`, `QTY`) VALUES('16','4','14','100');
+
+
+
+
+CREATE TABLE `seat` ( 
+ `ID` INT(11) NOT NULL, 
+ `SEAT_NO` INT(11) NOT NULL,
+ `STATUS` VARCHAR(50) NOT NULL,  PRIMARY KEY (`ID`));
+
+
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('1','1','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('2','2','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('3','3','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('4','4','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('5','5','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('6','6','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('7','7','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('8','8','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('9','9','AVAILABLE');
+INSERT INTO `seat` (`ID`, `SEAT_NO`, `STATUS`) VALUES('10','10','AVAILABLE');
+
+
+
+CREATE TABLE `ORDER_RECORDS` ( 
+ `ID` INT(200) NOT NULL , 
+ `SEAT_NO` INT(200) DEFAULT NULL, 
+ `FOOD_ITEM` VARCHAR(30) NOT NULL, 
+ `QTY` INT(10) NOT NULL,  
+`BILL_AMOUNT` BIGINT(30) DEFAULT NULL, 
+ `ORDER_DATE` DATETIME DEFAULT NULL, 
+ `STATUS` VARCHAR(30) DEFAULT NULL,  PRIMARY KEY (`ID`)) ;
---
+++ RESTAURANT_MAIN_ORDER.sql
@@ -0,0 +1,48 @@
+
+DROP PROCEDURE IF EXISTS PLACE_ORDER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE PLACE_ORDER(IN SEAT INT,IN ITEM MEDIUMTEXT,IN QUANTITY MEDIUMTEXT)
+    
+    BEGIN
+          DECLARE _next1 TEXT DEFAULT NULL ;
+          DECLARE _nextlen1 INT DEFAULT NULL;
+          DECLARE _value1 TEXT DEFAULT NULL;
+          DECLARE _next2 TEXT DEFAULT NULL ;
+          DECLARE _nextlen2 INT DEFAULT NULL;
+          DECLARE _value2 TEXT DEFAULT NULL;
+          DECLARE temp INT;
+          DECLARE ORDER_ID INT;
+          SET ORDER_ID=(FLOOR(100+RAND()*(900)));
+          SET temp=(LENGTH(ITEM)-LENGTH(REPLACE(ITEM,',','')))+1;
+          IF (temp<=5)
+          THEN 
+                                  
+          iterator :
+          LOOP    
+            IF LENGTH(TRIM(ITEM)) = 0 OR ITEM IS NULL OR LENGTH(TRIM(QUANTITY)) = 0 OR QUANTITY IS NULL THEN
+              LEAVE iterator;
+              END IF;   
+                 SET _next1 = SUBSTRING_INDEX(ITEM,',',1);
+                 SET _nextlen1 = LENGTH(_next1);
+                 SET _value1 = TRIM(_next1);
+                 
+                 SET _next2 = SUBSTRING_INDEX(QUANTITY,',',1);
+                 SET _nextlen2 = LENGTH(_next2);
+                 SET _value2 = TRIM(_next2);
+                 
+                 CALL ORDERS(ORDER_ID,SEAT,_next1,_next2);
+                
+                 SET ITEM = INSERT(ITEM,1,_nextlen1 + 1,'');
+                 SET QUANTITY = INSERT(QUANTITY,1,_nextlen2 + 1,'');
+         END LOOP; 
+         END IF;  
+          
+                 UPDATE  SEAT SET STATUS='AVAILABLE' WHERE SEAT_NO=SEAT;
+    END $$
+DELIMITER ;
+
+
+
+CALL PLACE_ORDER(3,'TEA,COFFE,IDLY','3,5,1')
